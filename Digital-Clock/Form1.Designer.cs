a0e860d6d65459434f6cfe59fc80d0bbac43e304
JordanT111596 <JordanT111596@gmail.com>
2021-03-03 15:16:52 -0500

    Basic form skeleton design created

--- Digital-Clock/Form1.Designer.cs
+++ Digital-Clock/Form1.Designer.cs
@@ -29,13 +29,94 @@ namespace Digital_Clock
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.secondsLabel = new System.Windows.Forms.Label();
+            this.calLabel = new System.Windows.Forms.Label();
+            this.dayLabel = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Segoe UI", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.ForeColor = System.Drawing.Color.White;
+            this.timeLabel.Location = new System.Drawing.Point(243, 105);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(284, 128);
+            this.timeLabel.TabIndex = 0;
+            this.timeLabel.Text = "03:06";
+            // 
+            // secondsLabel
+            // 
+            this.secondsLabel.AutoSize = true;
+            this.secondsLabel.Font = new System.Drawing.Font("Segoe UI", 24F);
+            this.secondsLabel.ForeColor = System.Drawing.Color.White;
+            this.secondsLabel.Location = new System.Drawing.Point(512, 172);
+            this.secondsLabel.Name = "secondsLabel";
+            this.secondsLabel.Size = new System.Drawing.Size(54, 45);
+            this.secondsLabel.TabIndex = 1;
+            this.secondsLabel.Text = "55";
+            // 
+            // calLabel
+            // 
+            this.calLabel.AutoSize = true;
+            this.calLabel.Font = new System.Drawing.Font("Segoe UI", 36F);
+            this.calLabel.ForeColor = System.Drawing.Color.White;
+            this.calLabel.Location = new System.Drawing.Point(48, 284);
+            this.calLabel.Name = "calLabel";
+            this.calLabel.Size = new System.Drawing.Size(372, 65);
+            this.calLabel.TabIndex = 2;
+            this.calLabel.Text = "March 3rd, 2021";
+            // 
+            // dayLabel
+            // 
+            this.dayLabel.AutoSize = true;
+            this.dayLabel.Font = new System.Drawing.Font("Segoe UI", 36F);
+            this.dayLabel.ForeColor = System.Drawing.Color.White;
+            this.dayLabel.Location = new System.Drawing.Point(463, 299);
+            this.dayLabel.Name = "dayLabel";
+            this.dayLabel.Size = new System.Drawing.Size(271, 65);
+            this.dayLabel.TabIndex = 3;
+            this.dayLabel.Text = "Wednesday";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Segoe UI", 48F);
+            this.titleLabel.ForeColor = System.Drawing.Color.Lime;
+            this.titleLabel.Location = new System.Drawing.Point(31, 19);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(743, 86);
+            this.titleLabel.TabIndex = 4;
+            this.titleLabel.Text = "Jordan\'s C# Digital Clock";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.dayLabel);
+            this.Controls.Add(this.calLabel);
+            this.Controls.Add(this.secondsLabel);
+            this.Controls.Add(this.timeLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Label secondsLabel;
+        private System.Windows.Forms.Label calLabel;
+        private System.Windows.Forms.Label dayLabel;
+        private System.Windows.Forms.Label titleLabel;
     }
 }
 
