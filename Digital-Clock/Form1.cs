e7eff8786ed9953f7a2f95c4337d7e5af106ea60
JordanT111596 <JordanT111596@gmail.com>
2021-03-04 13:59:02 -0500

    Commented code

--- Digital-Clock/Form1.cs
+++ Digital-Clock/Form1.cs
@@ -19,15 +19,21 @@ namespace Digital_Clock
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            //the time label is updated using DateTime to show the current hour and minute of the day
             timeLabel.Text = DateTime.Now.ToString("hh:mm");
+            //the seconds label is updated using DateTime to show the seconds of the current minute
             secondsLabel.Text = DateTime.Now.ToString("ss");
+            //the AM/PM label is updated using DateTime to show if it is currently AM or PM
             amPmLabel.Text = DateTime.Now.ToString("tt");
+            //the calendar label is updated using DateTime to show the current month, date, and year
             calLabel.Text = DateTime.Now.ToString("MMMM dd, yyyy");
+            //the day label is updated using DateTime to show the current day of the week
             dayLabel.Text = DateTime.Now.ToString("dddd");
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            //when the form loads, the timer begins and all labels are updated via the timer tick function
             timer.Start();
         }
     }
